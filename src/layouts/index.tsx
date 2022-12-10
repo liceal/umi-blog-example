6979c96878ac3ab811599cb9eb32bf8a9497e633
shougong <shougong@burgeon.cn>
2022-12-10 17:00:03 +0800

    fix

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,44 +1,55 @@
-import React from "react";
+import React, { useState, useEffect, MouseEventHandler } from "react";
 // @ts-ignore
-import { Outlet, history } from "umi";
+import { Outlet, history, useOutlet, History } from "umi";
 
-export default function Layout() {
-  return (
-    <div className="relative">
-      <div
-        className="fixed bottom-8 right-8 py-4 z-50 flex transition-all
+export default function Layout(props: {
+	children?: Array<{ slot?: { layoutLink: any } }>;
+}) {
+	const [buttons, setButtons] = useState([]);
+
+	return (
+		<div className="relative">
+			<div
+				className="fixed bottom-8 right-8 py-4 z-50 flex transition-all
         justify-end flex-col p-4 bg-white shadow hover:shadow-xl rounded"
-      >
-        {/* <p className="text-right text-xs">
-          这个博客是使用 <a href="https://next.umijs.org/zh-CN">Umi.js 框架</a>
-          <br />
-          搭配 <a href="https://planetscale.com/">PlanetScale</a> +{" "}
-          <a href="https://www.prisma.io/">Prisma </a>+
-          <a href="https://tailwindcss.com/">Tailwindcss</a> <br />
-          并且部署在 <a href="https://vercel.com/">Vercel</a> 的一个示例！
-        </p>
-        <p className="text-right mt-2">
-          <a
-            className="text-xs"
-            href="https://next.umijs.org/zh-CN/docs/tutorials/blog"
-          >
-            马上自己做一个 ➡️
-          </a>
-        </p> */}
-        <a href="#" onClick={() => history.push("/register")}>
-          注册
-        </a>
-        <a href="#" onClick={() => history.push("/login")}>
-          登录
-        </a>
-        <a href="#" onClick={() => history.push("/")}>
-          首页
-        </a>
-        <a href="#" onClick={() => history.push("/posts/create")}>
-          发表文章
-        </a>
-      </div>
-      <Outlet />
-    </div>
-  );
+			>
+				{/* <p className="text-right text-xs">
+					这个博客是使用 <a href="https://next.umijs.org/zh-CN">Umi.js 框架</a>
+					<br />
+					搭配 <a href="https://planetscale.com/">PlanetScale</a> +{" "}
+					<a href="https://www.prisma.io/">Prisma </a>+
+					<a href="https://tailwindcss.com/">Tailwindcss</a> <br />
+					并且部署在 <a href="https://vercel.com/">Vercel</a> 的一个示例！
+					</p>
+					<p className="text-right mt-2">
+					<a
+						className="text-xs"
+						href="https://next.umijs.org/zh-CN/docs/tutorials/blog"
+					>
+						马上自己做一个 ➡️
+					</a>
+					</p> */}
+				<a href="#" onClick={() => history.push("/register")}>
+					注册
+				</a>
+				<a href="#" onClick={() => history.push("/login")}>
+					登录
+				</a>
+				<a href="#" onClick={() => history.push("/")}>
+					首页
+				</a>
+				<a href="#" onClick={() => history.push("/posts/create")}>
+					发表文章
+				</a>
+				{buttons?.map((v: { click: any; text: string }, k) => {
+					return (
+						<a key={k} href="#" onClick={() => v.click(history)}>
+							{v.text}
+						</a>
+					);
+				})}
+			</div>
+			<Outlet context={{ methods: { setButtons } }} />
+		</div>
+	);
 }
--- src/pages/posts/post.tsx
+++ src/pages/posts/post.tsx
@@ -1,122 +1,150 @@
 import React, { useEffect, useState } from "react";
-// @ts-ignore
-import { useParams, history } from "umi";
+import { useParams, history, useOutletContext, History } from "umi";
 
-export default function PostPage() {
-  const params = useParams();
-  const [post, setPost] = useState<any>();
+export default function PostPage(props?: any) {
+	console.log(useOutletContext());
+	let { methods } = useOutletContext<{
+		methods: {
+			setButtons: ([]: Iterable<{
+				text: string;
+				click: (h: History) => void;
+			}>) => void;
+		};
+	}>();
 
-  async function refresh() {
-    try {
-      const res = await fetch("/api/posts/" + params.postId);
-      const post = await res.json();
-      if (res.status === 200) {
-        setPost(post);
-      } else {
-        setPost(null);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
+	const params = useParams();
+	const [post, setPost] = useState<any>();
 
-  useEffect(() => {
-    refresh();
-  }, []);
+	async function refresh() {
+		try {
+			const res = await fetch("/api/posts/" + params.postId);
+			const post = await res.json();
+			if (res.status === 200) {
+				setPost(post);
+			} else {
+				setPost(null);
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
-  if (post === null) {
-    return <div>Post with ID {params.postId} not found.</div>;
-  }
+	useEffect(() => {
+		refresh();
+		methods.setButtons([
+			{
+				text: "编辑",
+				click: (h) => h.push(`/posts/${params.postId}/edit`),
+			},
+		]);
 
-  return (
-    <div className="max-w-screen overflow-x-hidden">
-      {post === undefined && (
-        <div className="fixed w-screen h-screen flex justify-center items-center">
-          <p className="animate-pulse">Loading...</p>
-        </div>
-      )}
-      {post && (
-        <>
-          <div className="w-full lg:h-[46rem] h-[36rem] overflow-hidden relative flex justify-center">
-            <img
-              src={post.imageUrl}
-              className="absolute top-0 w-full h-full object-cover"
-              alt=""
-            />
-            <div className="w-full h-full absolute top-0 right-0 bg-black bg-opacity-60" />
-            <div className="w-full absolute lg:bottom-24 bottom-12 container lg:px-64 px-8">
-              <p className="text-white text-4xl font-extrabold">{post.title}</p>
-              <div className="flex flex-row mt-8 align-bottom">
-                <img
-                  src={post.author.avatarUrl}
-                  className="rounded-full h-8 w-8 mr-4"
-                  alt=""
-                />
-                <p className="text-white text-xl font-extrabold opacity-80">
-                  {post.author.name}
-                </p>
-                <p className="text-white text-xl ml-8 opacity-60">
-                  {post.createdAt.split("T")[0]}
-                </p>
-                <p className="text-white text-xl ml-8 bg-white bg-opacity-80 rounded-sm">
-                  <a
-                    href="#"
-                    onClick={() => history.push(`/posts/${params.postId}/edit`)}
-                  >
-                    编辑
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full flex justify-center my-24">
-            <div className="w-full container lg:px-64 px-8">
-              {post.content.split("\n").map((line: string, i: number) => {
-                if (line.startsWith("# "))
-                  return (
-                    <p key={i} className="text-3xl font-extrabold mt-24 mb-12">
-                      {line.split("# ")[1]}
-                    </p>
-                  );
-                if (line.startsWith("## "))
-                  return (
-                    <p key={i} className="text-2xl font-extrabold mt-24 mb-12">
-                      {line.split("## ")[1]}
-                    </p>
-                  );
-                if (line.startsWith("### "))
-                  return (
-                    <p key={i} className="text-xl font-extrabold mt-24 mb-12">
-                      {line.split("### ")[1]}
-                    </p>
-                  );
-                if (line.startsWith("![]("))
-                  return (
-                    <img
-                      src={line.split("![](")[1].split(")")[0]}
-                      className="w-full h-auto mb-12 mt-8 shadow-xl rounded"
-                      alt=""
-                      key={i}
-                    />
-                  );
-                if (line.startsWith("<"))
-                  return (
-                    <div
-                      key={i}
-                      dangerouslySetInnerHTML={{ __html: line }}
-                      className="mt-8 mb-12"
-                    />
-                  );
-                return (
-                  <p key={i} className="text-zinc-600 mt-2">
-                    {line}
-                  </p>
-                );
-              })}
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+		return () => methods.setButtons([]);
+	}, []);
+
+	if (post === null) {
+		return <div>Post with ID {params.postId} not found.</div>;
+	}
+
+	return (
+		<div className="max-w-screen overflow-x-hidden">
+			{post === undefined && (
+				<div className="fixed w-screen h-screen flex justify-center items-center">
+					<p className="animate-pulse">Loading...</p>
+				</div>
+			)}
+			{post && (
+				<>
+					<div className="w-full lg:h-[46rem] h-[36rem] overflow-hidden relative flex justify-center">
+						<img
+							src={post.imageUrl}
+							className="absolute top-0 w-full h-full object-cover"
+							alt=""
+						/>
+						<div className="w-full h-full absolute top-0 right-0 bg-black bg-opacity-60" />
+						<div className="w-full absolute lg:bottom-24 bottom-12 container lg:px-64 px-8">
+							<p className="text-white text-4xl font-extrabold">{post.title}</p>
+							<div className="flex flex-row mt-8 align-bottom">
+								<img
+									src={post.author.avatarUrl}
+									className="rounded-full h-8 w-8 mr-4"
+									alt=""
+								/>
+								<p className="text-white text-xl font-extrabold opacity-80">
+									{post.author.name}
+								</p>
+								<p className="text-white text-xl ml-8 opacity-60">
+									{post.createdAt.split("T")[0]}
+								</p>
+								{/* <p className="text-white text-xl ml-8 bg-white bg-opacity-80 rounded-sm">
+								<a
+									href="#"
+									// onClick={() => history.push(`/posts/${params.postId}/edit`)}
+									onClick={() => {
+									outletContext.methods.setButtons([
+										{
+										text: "编辑",
+										click: () =>
+											history.push(`/posts/${params.postId}/edit`),
+										},
+									]);
+									}}
+								>
+									编辑
+								</a>
+								</p> */}
+							</div>
+						</div>
+					</div>
+					<div className="w-full flex justify-center my-24">
+						<div className="w-full container lg:px-64 px-8">
+							{post.content.split("\n").map((line: string, i: number) => {
+								if (line.startsWith("# "))
+									return (
+										<p key={i} className="text-3xl font-extrabold mt-24 mb-12">
+											{line.split("# ")[1]}
+										</p>
+									);
+								if (line.startsWith("## "))
+									return (
+										<p key={i} className="text-2xl font-extrabold mt-24 mb-12">
+											{line.split("## ")[1]}
+										</p>
+									);
+								if (line.startsWith("### "))
+									return (
+										<p key={i} className="text-xl font-extrabold mt-24 mb-12">
+											{line.split("### ")[1]}
+										</p>
+									);
+								if (line.startsWith("![]("))
+									return (
+										<img
+											src={line.split("![](")[1].split(")")[0]}
+											className="w-full h-auto mb-12 mt-8 shadow-xl rounded"
+											alt=""
+											key={i}
+										/>
+									);
+								if (line.startsWith("<"))
+									return (
+										<div
+											key={i}
+											dangerouslySetInnerHTML={{
+												__html: line,
+											}}
+											className="mt-8 mb-12"
+										/>
+									);
+								return (
+									<p key={i} className="text-zinc-600 mt-2">
+										{line}
+									</p>
+								);
+							})}
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
